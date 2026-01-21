78ed3d610757b0be99f0eddc431b23be41144c2f
b4shfire <lorianrichmond@icloud.com>
2026-01-21 17:08:21 +0000

    various changes

--- pySecDecContrib/cnf.py
+++ pySecDecContrib/cnf.py
@@ -1,123 +1,131 @@
 #!/usr/bin/env python3
 
+#from os import environ
+#environ["TORCH_LOGS"] = "recompiles"
+
 from multiprocessing import shared_memory
 from time import sleep
 from datetime import datetime
 import numpy as np
-
+import time
 import torch
-from torch.distributions.uniform import Uniform
-from torchdiffeq import odeint
+from torchdiffeq import odeint_adjoint as odeint
 
 import matplotlib.pyplot as plt
 
 cuda = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+torch._dynamo.config.recompile_limit = 1000000
+torch._dynamo.config.accumulated_recompile_limit = 1000000
 
-#done_already2 = False
 
-def train_cnf(ndim, hidden_width=None, learn_rate=5e-3, batch_size=10000, epoch_size=10, num_epochs=10, cutoff_ess=0.99):
-    global done_already2
+def train_cnf(ndim):
+
     torch.manual_seed(0)
-    
-    if hidden_width is None:
-        hidden_width = 2**(ndim+1)
+
+    hidden_width = 16*ndim
+    batch_size = 10000
+    epoch_size = 10
+    num_epochs = 10
+    train_dtype=torch.float
+    eval_dtype=torch.double
     
     class CNF(torch.nn.Module):
         def __init__(self):
-          super(CNF, self).__init__()
-          self.fc1 = torch.nn.Linear(ndim+1, hidden_width)
-          self.fc2 = torch.nn.Linear(hidden_width, ndim)
-    
-        def forward(self, t, x):
-          y = self.fc1(torch.cat((torch.unsqueeze(t,0),x)))
-          y = torch.tanh(y)
-          y = self.fc2(y)
-          y *= torch.sin(x * torch.pi)
-          return y
+            super(CNF, self).__init__()
 
-    flow = CNF().double().to(cuda)
-    flow_jac = torch.func.jacrev(flow,1)
+            self.fc1 = torch.nn.Linear(ndim+1, hidden_width)
+            self.fc2 = torch.nn.Linear(hidden_width, ndim)
 
-    def flow_div(t, state):
-        v = flow(t, state[0])
-        div = torch.trace(flow_jac(t,state[0]))
-        return (v, div)
+            def v(t, x):
+                y = self.fc1(torch.cat((torch.unsqueeze(t,0),x)))
+                y = torch.tanh(y)
+                y = self.fc2(y)
+                y *= torch.sin(x * torch.pi)
+                return y
 
-    flow_div_batched = torch.vmap(flow_div, (None, (0, None)))
-    source_dist = Uniform(torch.zeros(ndim).to(cuda), torch.ones(ndim).to(cuda))
-    optim = torch.optim.Adam(flow.parameters(), lr=learn_rate)
+            jac = torch.func.jacrev(v,1)
+
+            def v_div(t, state):
+                return (v(t, state[0]), torch.trace(jac(t, state[0])))
+
+            self.forward = torch.vmap(v_div, (None, (0, None)))
+            self.forward = torch.compile(self.forward, dynamic=True)
+
+    flow = CNF().to(cuda, dtype=train_dtype)
+    rng = np.random.default_rng(0)
+    optim = torch.optim.Adam(flow.parameters(), lr=5e-3)
     
     batch_losses = []
     batch_ess = []
     best_ess = 0.
     best_flow_state = None
 
+    times = [[] for _ in range(8)]
+
     for batch in range(num_epochs * epoch_size):
         optim.zero_grad()
         
-        x = source_dist.sample([batch_size]).double().to(cuda)
+        times[0].append(time.perf_counter())
 
-        # perform korobov transform
-        p = x**3 * (1-x) ** 3 # for each dimension!
-        p = p.prod(dim=1)
-        x = x**4 * (-20.0*x**3 + 70.0*x**2 - 84.0*x + 35.0)
+        x_shared = np.ndarray((batch_size, ndim), np.float64, buffer=data.buf)
+        rng.random(x_shared.shape, out=x_shared)
+        x = torch.tensor(x_shared, dtype=train_dtype).to(cuda)
 
-        #if not done_already2:
-        #    done_already2 = True
-        #    plt.hist(x.cpu().detach().numpy()[:,0], bins=100)
-        #    plt.savefig("x.png")
-
-        res = odeint(flow_div_batched, (x,torch.zeros([batch_size]).to(cuda)), torch.tensor([0.,1.]).to(cuda), atol=1e-4)
-        y, lnJ = res[0][1], res[1][1]
-        y = torch.clamp(y, 0, 1)
-
-        eval_points = np.ndarray((batch_size, ndim), np.float64, buffer=data.buf)
-        eval_points[:,:] = y.cpu().detach().numpy()[:,:]
-        #print("points[0]: ", eval_points[0])
+        times[1].append(time.perf_counter())
 
         ctrl.buf[8:16] = batch_size.to_bytes(8, byteorder="little")
         ctrl.buf[0] = 3
-
         while ctrl.buf[0] == 3:
             sleep(0.01)
-        
         if ctrl.buf[0] != 4:
-            print("invalid state", ctrl.buf[0])
-            exit(1)
+            raise ValueError(f"invalid state: {ctrl.buf[0]}")
 
-        target_samples = torch.tensor(np.ndarray((batch_size), np.float64, buffer=data.buf)).to(cuda)
-        #print("target_samples[5]: ", target_samples[5].cpu().detach().numpy())
+        times[2].append(time.perf_counter())
 
-        sample_losses = -lnJ - torch.log(target_samples)
-        sample_losses -= torch.log(p) # add korobov transform loss
+        f_shared = np.ndarray((batch_size), np.float64, buffer=data.buf)
+        f = torch.tensor(f_shared, dtype=train_dtype).to(cuda)
+
+        times[3].append(time.perf_counter())
+
+        res = odeint(flow, (x,torch.zeros([batch_size], dtype=train_dtype).to(cuda)), torch.tensor([1.,0.], dtype=train_dtype).to(cuda), atol=1e-2)
+        ln_p = res[1][1]
+
+        times[4].append(time.perf_counter())
+
+        sample_losses = -f*ln_p
         batch_loss = torch.mean(sample_losses)
-        batch_losses.append(batch_loss.cpu().detach().numpy())
+        batch_losses.append(batch_loss.cpu().detach().float().numpy())
 
-        prop = torch.exp(-sample_losses) #target_samples / torch.exp(-lnJ)
-        ess = torch.mean(prop)**2/torch.mean(prop**2)
-        batch_ess.append(ess.cpu().detach().numpy())
+        batch_ess.append(np.mean(f_shared)**2/np.mean(f_shared**2/np.exp(ln_p.cpu().detach().float().numpy())))
 
-        ess_val = float(ess.detach().cpu().item())
-        if np.isfinite(ess_val) and ess_val > best_ess:
-            best_ess = ess_val
+        if batch_ess[-1] > best_ess:
+            best_ess = batch_ess[-1]
             best_flow_state = {k: v.detach().cpu().clone() for k, v in flow.state_dict().items()}
 
-        if batch_ess[-1] < cutoff_ess:
-            batch_loss.backward()
-            optim.step()
+        times[5].append(time.perf_counter())
 
-        if (batch == 0) or ((batch+1) % epoch_size == 0) or (batch_ess[-1] >= cutoff_ess):
+        batch_loss.backward()
+
+        times[6].append(time.perf_counter())
+
+        optim.step()
+
+        times[7].append(time.perf_counter())
+
+        if (batch == 0) or ((batch+1) % epoch_size == 0):
 
             print(f"Epoch {batch // epoch_size} batch {batch % epoch_size} loss: {batch_losses[-1]}")
             print(f"Epoch {batch // epoch_size} batch {batch % epoch_size} ESS: {batch_ess[-1]}")
 
-            if batch_ess[-1] >= cutoff_ess:
-                break
-
     if best_flow_state is not None:
         flow.load_state_dict(best_flow_state)
         print(f"Best ESS={best_ess}")
+
+    times = [np.array(t)[1:] for t in times]
+
+    for i in range(1, len(times)):
+        print(f"[Train] Time taken for {i}: {np.sum(times[i] - times[i-1])}")
 
     # plot loss and ESS
     plt.clf()
@@ -133,14 +141,16 @@ def train_cnf(ndim, hidden_width=None, learn_rate=5e-3, batch_size=10000, epoch_
     ax2.legend()
     plt.savefig(f"loss_plots/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.pdf")
 
+    # switch to doubles for evaluation
+    flow = flow.to(cuda, dtype=eval_dtype)
+
     def forward_points(x):
         with torch.no_grad():
-            res = odeint(flow_div_batched, (x,torch.zeros(x.shape[0]).to(cuda)), torch.tensor([0.,1.]).to(cuda), atol=1e-4)
+            res = odeint(flow, (x,torch.zeros(x.shape[0]).to(cuda, dtype=eval_dtype)), torch.tensor([0.,1.]).to(cuda, dtype=eval_dtype), atol=1e-8, rtol=0)
             return res[0][1], torch.exp(-res[1][1])
-    
-    return forward_points, batch_losses
 
-done_already = False
+    return forward_points
+
 
 def run():
 
@@ -150,17 +160,24 @@ def run():
         exit(1)
 
     ndim = int.from_bytes(ctrl.buf[8:16], byteorder="little")
-    print(f"ndim={ndim}")
+    print(f"\nndim={ndim}")
 
-    forward_points, batch_losses = train_cnf(ndim)
+    forward_points = train_cnf(ndim)
+
+    times = [[] for _ in range(4)]
 
     while True:
+
+        times[0].append(time.perf_counter())
 
         ctrl.buf[0] = 5
         while ctrl.buf[0] == 5:
             sleep(0.01)
-
         if ctrl.buf[0] == 2:
+            del times[0][-1]
+            times = [np.array(t)[1:] for t in times]
+            for i in range(len(times)-1):
+                print(f"[Eval] Time taken for {i}: {np.sum(times[i+1] - times[i])}")
             return run() # restart
         elif ctrl.buf[0] != 6:
             print("invalid state", ctrl.buf[0])
@@ -168,21 +185,19 @@ def run():
 
         n = int.from_bytes(ctrl.buf[8:16], byteorder="little")
         x = torch.tensor(np.ndarray((n, ndim), np.float64, buffer=data.buf)).to(cuda)
-        y, p = forward_points(x)
 
+        times[1].append(time.perf_counter())
+
+        y, p = forward_points(x)
         y = torch.clamp(y, 0, 1)
 
-        if not done_already:
-            done_already = True
-
-            #plt.hist(x.cpu().detach().numpy()[:,0], bins=100)
-            #plt.savefig("x2.png")
-            plt.clf()
-            plt.hist(y.cpu().detach().numpy()[:,0], bins=100)
-            plt.savefig("y2.png")
+        times[2].append(time.perf_counter())
 
         res = np.ndarray((n, ndim+1), np.float64, buffer=data.buf)
         res[:,:] = np.concatenate((p.cpu().detach().numpy()[:,None], y.cpu().detach().numpy()[:,:]), axis=1)
+
+        times[3].append(time.perf_counter())
+    
 
 
 def main():
